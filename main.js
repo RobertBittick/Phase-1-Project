ecf358d65585f61f191fafd947ab81e52bca79ba
Robert Bittick <robertbittick@gmail.com>
2022-11-08 17:49:28 -0800

    commit message

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ select.addEventListener('change', () => {
     fetch('http://localhost:3000/Events')
     .then(response => response.json())
     .then(function(data) {
-            let option;
+        console.log(data.id)
+            // if(data.id)
             for(let i = 0; i < data.length; i++) {
 
                 let img = document.createElement('img')
-                img.setAttribute('src', el.image)
+                img.setAttribute('src', data.image)
 
                 let h2 = document.createElement('h2')
-                h2.innerText = el.name
+                h2.innerText = data.name
 
                 let p = document.createElement('p')
-                p.innerText = el.info
+                p.innerText = data.info
 
                 let card = document.createElement('div')
                 card.setAttribute('class','card')
@@ -31,22 +32,6 @@ select.addEventListener('change', () => {
     }) // end .then
 
    
-    //buildCard()
 }) //end event listener
-
-        let img = document.createElement('img')
-        img.setAttribute('src', el.image)
-
-        let h2 = document.createElement('h2')
-        h2.innerText = el.name
-
-        let p = document.createElement('p')
-        p.innerText = el.info
-
-        let card = document.createElement('div')
-        card.setAttribute('class','card')
-        card.append(img,h2,p)
-
-        container.append(card)
 
 })
