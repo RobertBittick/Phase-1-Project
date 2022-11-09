c7e103232e509ac11b61fba9769f718d381ebb0f
Robert Bittick <robertbittick@gmail.com>
2022-11-08 21:08:22 -0800

    commit message

--- main.js
+++ main.js
@@ -2,31 +2,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const select = document.getElementById('event-dropdown')
+const selectedVal = document.getElementById('event-dropdown').value
 const container = document.getElementById('event-lists')
+
 
 select.addEventListener('change', () => {
     fetch('http://localhost:3000/Events')
     .then(response => response.json())
     .then(function(data) {
-        console.log(data.id)
-            // if(data.id)
-            for(let i = 0; i < data.length; i++) {
+            data.map(element =>{
+                console.log(element)
+                console.log(selectedVal)
+                
+                if(element.id === selectedVal) {
 
-                let img = document.createElement('img')
-                img.setAttribute('src', data.image)
+                    let img = document.createElement('img')
+                    img.setAttribute('src', element.image)
 
-                let h2 = document.createElement('h2')
-                h2.innerText = data.name
+                    let h2 = document.createElement('h2')
+                    h2.innerText = element.name
 
-                let p = document.createElement('p')
-                p.innerText = data.info
+                    let p = document.createElement('p')
+                    p.innerText = element.info
 
-                let card = document.createElement('div')
-                card.setAttribute('class','card')
-                card.append(img,h2,p)
+                    let card = document.createElement('div')
+                    card.setAttribute('class','card')
+                    card.append(img,h2,p)
 
-                container.append(card)
-            }   
+                    container.append(card)
+                }
+            })   
             
         
     }) // end .then
