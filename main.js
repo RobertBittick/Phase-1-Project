5abf88fb1f916696f36bdd49bb49a54523036cc5
Robert Bittick <robertbittick@gmail.com>
2022-11-08 17:46:09 -0800

    commit message

--- main.js
+++ main.js
@@ -2,24 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const select = document.getElementById('event-dropdown')
+const container = document.getElementById('event-lists')
 
 select.addEventListener('change', () => {
     fetch('http://localhost:3000/Events')
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        data.map(element => {
-            eventArray =[]
-            eventArray.push(element)
-            buildCard(element[1])
-
-            
-        }) // end of data.map
+    .then(function(data) {
+            let option;
+            for(let i = 0; i < data.length; i++) {
+                option = document.createElement('option');
+      	        option.text = data[i].name;
+      	        option.value = data[i].abbreviation;
+      	        select.add(option);
+            }   
             
         
     }) // end .then
 
-    function buildCard(el) {
+   
+    //buildCard()
+}) //end event listener
+
+ function buildCard(el) {
         let img = document.createElement('img')
         img.setAttribute('src', el.image)
 
@@ -32,9 +36,8 @@ select.addEventListener('change', () => {
         let card = document.createElement('div')
         card.setAttribute('class','card')
         card.append(img,h2,p)
-    }
-    buildCard()
-}) //end event listener
 
+        container.append(card)
+    }
 
 })
