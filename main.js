542f3e00dbe584064bc1213a98688e80dd7748b7
Robert Bittick <robertbittick@gmail.com>
2022-11-08 17:47:00 -0800

    commit message

--- main.js
+++ main.js
@@ -10,10 +10,21 @@ select.addEventListener('change', () => {
     .then(function(data) {
             let option;
             for(let i = 0; i < data.length; i++) {
-                option = document.createElement('option');
-      	        option.text = data[i].name;
-      	        option.value = data[i].abbreviation;
-      	        select.add(option);
+
+                let img = document.createElement('img')
+                img.setAttribute('src', el.image)
+
+                let h2 = document.createElement('h2')
+                h2.innerText = el.name
+
+                let p = document.createElement('p')
+                p.innerText = el.info
+
+                let card = document.createElement('div')
+                card.setAttribute('class','card')
+                card.append(img,h2,p)
+
+                container.append(card)
             }   
             
         
@@ -23,7 +34,6 @@ select.addEventListener('change', () => {
     //buildCard()
 }) //end event listener
 
- function buildCard(el) {
         let img = document.createElement('img')
         img.setAttribute('src', el.image)
 
@@ -38,6 +48,5 @@ select.addEventListener('change', () => {
         card.append(img,h2,p)
 
         container.append(card)
-    }
 
 })
