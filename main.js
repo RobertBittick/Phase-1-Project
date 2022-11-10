3fa19685dee071f6ee9af1f78369197a110714b6
Robert Bittick <robertbittick@gmail.com>
2022-11-10 12:15:11 -0800

    commit message

--- main.js
+++ main.js
@@ -2,43 +2,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const select = document.getElementById('event-dropdown')
-const selectedVal = document.getElementById('event-dropdown').value
+const selectedVal = select.options[select.selectedIndex].text
+
 const container = document.getElementById('event-lists')
 
 
 select.addEventListener('change', () => {
     fetch('http://localhost:3000/Events')
     .then(response => response.json())
-    .then(function(data) {
+    .then(data => {
             data.map(element =>{
                 console.log(element.id)
                 console.log(selectedVal)
-                
-                switch(element.id) {
-                    case 1:
-                        let img = document.createElement('img')
-                        img.setAttribute('src', element.image)
-
-                        let h2 = document.createElement('h2')
-                        h2.innerText = element.name
-
-                        let p = document.createElement('p')
-                        p.innerText = element.info
-
-                        let card = document.createElement('div')
-                        card.setAttribute('class','card')
-                        card.append(img,h2,p)
-
-                        container.append(card) 
-                        break
-                    }
-                })
-                
-            })   
+                buildCard(element)
+            })
+        }) 
             
         
     }) // end .then
 
+    function buildCard(element){
+        let img = document.createElement('img')
+        img.setAttribute('src', element.image)
+
+        let h2 = document.createElement('h2')
+        h2.innerText = element.name
+
+        let p = document.createElement('p')
+        p.innerText = element.info
+
+        let card = document.createElement('div')
+        card.setAttribute('class','card')
+        card.append(img,h2,p)
+
+        container.append(card) 
+    }           
    
 }) //end event listener
 
