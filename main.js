00f1735e5cee90dd3e9e987a97ce168dcdf8c59a
Robert Bittick <robertbittick@gmail.com>
2022-11-12 22:26:33 -0800

    commit message

--- main.js
+++ main.js
@@ -33,6 +33,11 @@ let mapObjects
         img.setAttribute('src',element.image)
         img.className = 'map-avatar'
 
+        let button = document.createElement('button')
+        button.innerText = 'Like ❤️'
+        button.className = 'like-btn'
+        button.setAttribute('id', element.id)
+        button.addEventListener('click', (e) => likes(e))
 
         let h2 = document.createElement('h2')
         h2.innerText = element.name
@@ -40,14 +45,37 @@ let mapObjects
         let p = document.createElement('p')
         p.innerText = element.info
 
+        let p2 = document.createElement('p')
+        p2.innerText = `${element.likes}`
+
+
         let card = document.createElement('div')
         card.setAttribute('class','card')
         card.height = 300
         card.width = 600
-        card.append(img,h2,p)
+        card.append(img,h2,p,p2,button)
 
         container.append(card) 
     }           
    
+
+    function likes (event) {
+        let more = parseInt(event.target.previousElementSibling.innerText) + 1
+      fetch(`http://localhost:3000/maps/${event.target.id}`, { 
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            'likes': 0
+          })
+        })  
+        .then(response => response.json())
+        .then(data => {
+          event.target.previousElementSibling.innerText= `${more} likes`
+        })
+      }
+    
 })
 
