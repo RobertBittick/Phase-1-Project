7b23325f26b0d8589522975497fa33d686a28e1d
Robert Bittick <robertbittick@gmail.com>
2022-11-14 19:49:39 -0800

    Built out some of the ReadME

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-This is my readme file
+# A History of Rome in Maps
+
+## Project Purpose and my Goal
+The purpose of this project was to use the knowledge that I learn so far to build a simple web application. This project was a test of my abilities to think critically and put together the sum of what I've learned in Phase 1 of my journey with Flatiron. As my first project from scratch, the challenge was more than I expected. With that in mind, I learned a lot about HTML and CSS as well as Javascript that I wouldn't have without hands on trial and error. This project exposed my weaknesses as well as my strengths and showed me what I needed to work on.
+
+My goal with this project was to build a simple web application that I could be proud of. I took some things that I enjoy immensely, history and maps, and using my favorite time period I build a web application that, despite it's simplicity, I am proud of. I greatly enjoy talking about history and geography, and I wanted to share my passion with others through this web application
+
+## Contributors
+
+- Robert Bittick
+
+## How to use
+*A History of Rome in Maps* is a simple web application consisting of a single page. Below the overview, you can use the drop down to select a time period to view a map, and read a short synopsis about that particular time period. There is a section to add general comments about the content or whatever you please. In addition there is a like button to show that you enjoyed a particular map or that you appreciated some of the information. 
+
+## My Challenges
+
+## Final Conclusion
+
+
+
+
+
+
+#### Sources
+
--- main.js
+++ main.js
@@ -1,92 +1,87 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const select = document.getElementById('event-dropdown')
+  const container = document.getElementById('event-lists')
+  const submit = document.querySelector('form')
+  const commentList = document.querySelectorAll('.comments')
+  let mapObjects
 
+  function getMaps(){
+    fetch('http://localhost:3000/maps')
+    .then(response => response.json())
+    .then(data => {
+      mapObjects = data
+    })
+  }
 
-const select = document.getElementById('event-dropdown')
+  getMaps()
 
-const container = document.getElementById('event-lists')
+  submit.addEventListener('submit', (e) => addComment(e))
 
-const submit = document.querySelector('form')
+  select.addEventListener('change', () => {
+      const selectedId = parseInt(select.value)
+      mapObjects.find((mapObject) => {
+          if(mapObject.id === selectedId){
+            buildCard(mapObject)
+          }
+      })
+  }) 
 
-const commentList = document.querySelectorAll('.comments')
+  function buildCard(element){
 
-let mapObjects
+    let img = document.createElement('img')
+    img.setAttribute('src',element.image)
+    img.className = 'map-avatar'
 
-submit.addEventListener('submit', (e) => addComment(e))
+    let button = document.createElement('button')
+    button.innerText = 'Like ❤️'
+    button.className = 'like-btn'
+    button.setAttribute('id', element.id)
+    button.addEventListener('click', (e) => likes(e))
 
-    function getMaps(){
-        fetch('http://localhost:3000/maps')
-        .then(response => response.json())
-        .then(data => {
-            mapObjects = data
-        })
-    }
+    let h2 = document.createElement('h2')
+    h2.innerText = element.name
 
-    getMaps()
+    let p = document.createElement('p')
+    p.innerText = element.info
 
-    select.addEventListener('change', () => {
-        const selectedId = parseInt(select.value)
-        mapObjects.find((mapObject) => {
-            if(mapObject.id === selectedId){
-                buildCard(mapObject)
-            }
-        })
-    }) 
-
-    function buildCard(element){
-
-        let img = document.createElement('img')
-        img.setAttribute('src',element.image)
-        img.className = 'map-avatar'
-
-        let button = document.createElement('button')
-        button.innerText = 'Like ❤️'
-        button.className = 'like-btn'
-        button.setAttribute('id', element.id)
-        button.addEventListener('click', (e) => likes(e))
-
-        let h2 = document.createElement('h2')
-        h2.innerText = element.name
-
-        let p = document.createElement('p')
-        p.innerText = element.info
-
-        let p2 = document.createElement('p')
-        p2.innerText = `${element.likes} likes`
+    let p2 = document.createElement('p')
+    p2.innerText = `${element.likes} likes`
         
-        let card = document.createElement('div')
-        card.setAttribute('class','card')
-        card.height = 300
-        card.width = 600
-        card.append(img,h2,p,p2,button)
+    let card = document.createElement('div')
+    card.setAttribute('class','card')
+    card.height = 300
+    card.width = 600
+    card.append(img,h2,p,p2,button)
 
-        container.append(card) 
-    }           
+    container.append(card) 
+  }           
    
 
-    function likes(event) {
-        let more = parseInt(event.target.previousElementSibling.innerText) + 1
-      fetch(`http://localhost:3000/maps/${event.target.id}`, { 
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          body: JSON.stringify({
-            'likes': more
-          })
-        })  
-        .then(response => response.json())
-        .then(data => {
-          event.target.previousElementSibling.innerText= `${more} likes`
+  function likes(event) {
+    let more = parseInt(event.target.previousElementSibling.innerText) + 1
+    fetch(`http://localhost:3000/maps/${event.target.id}`, { 
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+        body: JSON.stringify({
+          'likes': more
         })
-      }
+    })  
+    .then(response => response.json())
+    .then(data => {
+      event.target.previousElementSibling.innerText= `${more} likes`
+    })
+  }
 
-      function addComment(e) {
-        e.preventDefault()
-        const newComment = document.createElement('li')
-        newComment.textContent = e.target['comment-input'].value
-        commentList[0].appendChild(newComment)
-        submit.reset()
-        }
+  function addComment(e) {
+    e.preventDefault()
+    const newComment = document.createElement('li')
+    newComment.textContent = e.target['comment-input'].value
+    commentList[0].appendChild(newComment)
+    submit.reset()
+  }
+
 })
 
